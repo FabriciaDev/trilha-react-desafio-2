fd001f13bfc45f5746008ab9a3fdd93d1ed89b7e
FabriciaDev <phabymandalas@gmail.com>
2022-12-07 21:35:13 -0300

    final2

--- src/pages/App.js
+++ src/pages/App.js
@@ -36,7 +36,9 @@ function App() {
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
 
-    // utilizar filter.
+    const remove = repos.filter((repo) => repo.id !== id);
+    setRepos(remove);
+    
   }
 
 
